70f65e86f7ac9e865a173c4b79cc54d20ca64c7a
Schaefer <benjamin.schaefer@huk-coburg.de>
2025-01-16 09:57:48 +0100

    add service and other stuff

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
-
+import { HousingLocation } from "../housinglocation";
+import { HousingService } from "../housing.service";
 @Component({
   selector: "app-home",
-  imports: [HousingLocationComponent],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
@@ -12,9 +14,20 @@ import { HousingLocationComponent } from "../housing-location/housing-location.c
       </form>
     </section>
     <section class="results">
-      <app-housing-location></app-housing-location>
+      <app-housing-location
+        *ngFor="let housingLocation of housingLocationList"
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
     </section>
   `,
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public housingLocationList: HousingLocation[] = [];
+
+  private housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+}
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   imports: [],
   template: `
-    <p>
-      housing-location works!
-    </p>
+<section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+    </section>
   `,
-  styles: ``
+  styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
-
+@Input() housingLocation!: HousingLocation;
 }
